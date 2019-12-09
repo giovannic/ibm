2cfe39b2fa2d9ebb67597b1d85ee9b597cece04e
Giovanni <giovanni.charles@gmail.com>
2019-12-09 13:28:02 +0000

    Implement human force of infection

--- process_utility_functions.r
+++ process_utility_functions.r
@@ -4,16 +4,38 @@ package(dployr)
 # Human functions
 # ===============
 
-force_of_infection <- function(age, timestep) { #Not correct
-  # Calculate the unique biting rate (psi) from age
-  # Calculate the mean EIR (epsilon0) from time (how??)
-  # Sample the relative biting rate (xi) from a normal distribution
-  # Calculate immunity level (b) (why?? isn't this part of phi?)
-  #TODO: implement
-  return rep(1, length(timestep))
+# Calculate the unique biting rate (psi) from age
+# Calculate the mean EIR (epsilon0) from time (how??)
+# Sample the relative biting rate (xi) from a normal distribution
+# Calculate immunity level (b)
+force_of_infection <- function(
+  age,
+  v,
+  xi,
+  mosquito_state,
+  ib,
+  timestep,
+  parameters
+  ) {
+
+  psi <- unique_biting_rate(age)
+  _pi <- (xi * psi) / sum(xi * psi)
+
+  # Prepare mosquito frame
+  mosquito_frame <- data.frame(variant=v, state=mosquito_state)
+  infectious_count <- mosquito_frame[which(mosquito_frame$state == 'Im')] %>%
+    groupby("variant") %>% tally()
+  infectious_count$blood_meal_rate <- blood_meal_rate(
+    infectious_count$variant,
+    parameters
+  )
+
+  epsilon0 <- _pi * sum(infectious_count$blood_meal_rate * infectious_count$n)
+  b <- infection_probability(ib, parameters)
+  return epsilon0 * xi * b * psi
 }
 
-immunity <- function(age, timestep_last_bitten) { #Not correct
+immunity <- function(age, timestep_last_bitten) {
   # Calculate acquired immunity from last_bitten
   # Calculate and maternal immunity from age
   # Then calculate immunity using parameters
@@ -21,28 +43,30 @@ immunity <- function(age, timestep_last_bitten) { #Not correct
   return rep(1, length(timestep_last_bitten))
 }
 
-
 # Unique biting rate (psi) for a human of a given age
-unique_biting_rate <- function(age) {
-  return 1 - rho * exp(- age / a0)
+unique_biting_rate <- function(age, parameters) {
+  return 1 - parameters$rho * exp(- age / a0)
 }
 
 # Relative biting rate (xi) drawn from log normal
-relative_biting_rate <- function(n) {
-  return rlnorm(n, -sigma**2/2,sigma**2)
+relative_biting_rate <- function(n, parameters) {
+  return rlnorm(n, -parameters$sigma**2/2,parameters$sigma**2)
+}
+
+infection_probability <- function(ib, parameters) {
+  return parameters$bmin + (parameters$bmax - parameters$bmin) /
+    (1 + (ib / parameters$ib0)**parameters$kb)
 }
 
 # ==================
 # Mosquito functions
 # ==================
 
-mosquito_force_of_infection <- function(v, timestep) {
+# Implementd from Griffin et al 2010 S1 page 6 (should it be page 7?)
+# TODO: change to page 7 formulation
+mosquito_force_of_infection <- function(v, age, state, xi, timestep, parameters) {
 
   # Prepare data frame
-  age <- time_delayed_human_age$get_value() #TODO: move variable extraction into the model code
-  state <- time_delayed_human_state$get_value()
-  xi <- time_delayed_human_xi$get_value()
-
   human_frame <- data.frame(
     "age" = age,
     "state" = state
@@ -51,29 +75,32 @@ mosquito_force_of_infection <- function(v, timestep) {
 
   summary <- human_frame %>% group_by(.dots=c("state", "age", "xi")) %>% tally()
 
-  summary$constant = recode(
+  summary$infectivity = recode(
     human_frame$state,
-    D=cd, #TODO: move global parameters to function parameters
-    Treated=ct,
-    A=ca,
-    U=cu
+    D=parameters$cd,
+    Treated=parameters$ct,
+    A=parameters$ca,
+    U=parameters$cu
   )
 
   summary$psi <- unique_biting_rate(summary$age) * summary$xi
 
   #Calculate integral
-  integral = sum(summary$xi * summary$psi * summary$constant * summary$n)
+  integral = sum(summary$xi * summary$psi * summary$infectivity * summary$n)
 
   # Calculate normaliser
   age_distribution <- human_frame %>% groupby("age") %>% tally()
   age_distribution$p <- age_distribution$n / length(human_frame)
   omega <- sum(age_distribution$p * unique_biting_rate(age_distribution$age))
 
-  forces = recode(
+  return (blood_meal_rate(v, parameters) / omega) * integral
+}
+
+blood_meal_rate <- function(v, parameters) {
+  return recode(
     v,
-    `1`=(av1/omega)*integral,
-    `2`=(av2/omega)*integral,
-    `3`=(av3/omega)*integral
+    `1`=parameters$av1,
+    `2`=parameters$av2,
+    `3`=parameters$av3
   )
-  return forces
 }
